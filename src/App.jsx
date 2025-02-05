7cf009deccc38bf536e3978aaca692949ced7f4b
Tobless-scripts <obayomitaofeek7@gmail.com>
2025-02-05 23:41:38 +0100

    Updated App.jsx file

--- src/App.jsx
+++ src/App.jsx
@@ -1,22 +1,35 @@
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { BrowserRouter, Routes, Route, useLocation } from "react-router-dom";
+import { useEffect } from "react";
 import Home from "./components/Home";
 import About from "./components/About";
 import Contact from "./components/Contact";
 import Service from "./components/Services";
 import Blog from "./components/Blog";
 
+function AppContent() {
+    const location = useLocation();
+
+    useEffect(() => {
+        window.scrollTo(0, 0); // Scroll to the top whenever the route changes
+    }, [location.pathname]);
+
+    return (
+        <Routes>
+            <Route index element={<Home />} />
+            <Route path="/home" element={<Home />} />
+            <Route path="/about" element={<About />} />
+            <Route path="/service" element={<Service />} />
+            <Route path="/contact" element={<Contact />} />
+            <Route path="/blog" element={<Blog />} />
+        </Routes>
+    );
+}
+
 function App() {
     return (
         <div>
             <BrowserRouter>
-                <Routes>
-                    <Route index element={<Home />} />
-                    <Route path="/home" element={<Home />} />
-                    <Route path="/about" element={<About />} />
-                    <Route path="/service" element={<Service />} />
-                    <Route path="/contact" element={<Contact />} />
-                    <Route path="/blog" element={<Blog />} />
-                </Routes>
+                <AppContent />
             </BrowserRouter>
         </div>
     );
